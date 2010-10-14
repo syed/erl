913e1142a3cbe6e76a448bda7f3da2dbc2f64b0e
Syed <syedam@whoseonefind-lm.(none)>
2010-10-14 22:23:46 +0530

    Added merge sort

--- book/ch3/sorting.erl
+++ book/ch3/sorting.erl
@@ -39,8 +39,48 @@ list_merge([H|T],L2) ->
 
 % merge sort 
 
-mergesort(List)->
-    [].
+mergesort([])->
+    [];
+
+mergesort([H|[]]) -> 
+    [H];	 
+	     
+mergesort(L) ->
+    { L1 , L2 } = split(L),
+    io:format("{ L1 , L2 } = { ~p , ~p } ~n" , [L1,L2]),
+    merge(mergesort(L1) , mergesort(L2) ).
+
+
+% split list into 2 parts 
+
+split(L) ->
+    split(L , {[] ,[]}).
+
+
+% split does not necessarly have to be 1st half and second half
+% you can split by alternatively putting elements in two lists 
+
+split([] , { X , Y } ) ->
+    {X , Y};
+split([H|T] ,{ X , Y }) ->
+    split(T , {Y , append(X , [H])} ).
+
+append(A,B) ->
+    A ++ B .
+
+
+
+merge([],L2) ->
+    L2;
+merge(L1,[]) ->
+    L1;
+
+merge([H1|T1],[H2|T2]) when H1 >=  H2  ->
+    [H2|merge([H1|T1],T2)];
+merge([H1|T1],[H2|T2]) when H2 > H1 ->
+    [H1|merge(T1,[H2|T2])].
+
+
 
 % A very short implementation of quick sort ( copied from wikipedia :P) 
 
@@ -51,5 +91,6 @@ qsort([Pivot|T]) ->
     qsort([Front|| Front <- T, Front < Pivot])
     ++ [Pivot] ++ 
     qsort([Back||Back <- T , Back >= Pivot ]).
+
 
 
