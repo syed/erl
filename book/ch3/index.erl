4f5fdb64471eb3a18296a2a4f7ca250da904136f
Syed <syedam@whoseonefind-lm.(none)>
2010-10-17 08:21:34 +0530

    Stores index now

--- book/ch3/index.erl
+++ book/ch3/index.erl
@@ -5,8 +5,8 @@
 open_file(Filename) ->
     {ok,Fd} = file:open(Filename,[read]),
     RawList = raw_document(Fd),
-    ListOfWords = document(RawList).
-
+    ListOfWords = document(RawList),
+    index(ListOfWords,1,[]).
 
 
 % reads a line from a file and stores it in a list 
@@ -30,6 +30,31 @@ document([H|T]) ->
     % Head is a string , break into words 
     Words = string:tokens(H," \n"),
     Words ++ document(T).
+
+% Reads list of words and returns a tuple with word and occurences 
+
+index([],_,Tup)->
+    Tup;
+
+index([W|T],Count,Tup) ->
+    T2 = find_and_insert(W,Count,Tup),
+    %io:format("find_and_insert gave ~p ~n:" ,[T2]),
+    index(T,Count+1,T2).
+
+
+find_and_insert(W,Count,[])->
+    % Did not find in the tuple , Insert it 
+    [{W, [Count]}];
+
+find_and_insert(W,Count,[{W1,L1} |T]) when W1 == W ->
+    T2 = [{W,[Count|L1]} | T ],
+    T2;
+
+find_and_insert(W,Count,[H|T]) ->
+    [H| find_and_insert(W,Count,T)].
+
+
+	    
 
 
     
