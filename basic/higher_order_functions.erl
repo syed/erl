804fefbee53bbcf503618474dd97a7211ada1485
Syed <syed1.mushtaq@gmail.com>
2012-09-12 23:40:27 +0530

    Add code for implementing simple filter

--- basic/higher_order_functions.erl
+++ basic/higher_order_functions.erl
@@ -1,11 +1,12 @@
 -module(higher_order_functions).
--export([main/1,inc/1,dec/1]). %exporting inc,dec to make compiler happy
+-export([main/1,inc/1,dec/1,even_check/1]). %exporting inc,dec,even_check to make compiler happy
 
 
 main([]) ->
     % we are applying a map to inc and dec on a list
     io:format("incremented [1,2,3,4] ~p~n", [map(fun higher_order_functions:inc/1 , [1,2,3,4])]),
     io:format("decremented [1,2,3,4] ~p~n", [map(fun higher_order_functions:dec/1 , [1,2,3,4])]),
+
     %io:fomrat("decremented [1,2,3,4] ~p~n", map(fun higher_order_functions:dec/1 , [1,2,3,4])).
     %% Anonymous functions %%
     % fun(Args) ->
@@ -16,6 +17,7 @@ main([]) ->
     %       Expr1,Expr2....
     % end
     %
+
     io:format("incremented [1,2,3,4] ( anon func ) ~p~n", [map(fun(X) -> X+1 end  , [1,2,3,4])]),
 
     % Closures , access variables of parent function 
@@ -30,8 +32,27 @@ main([]) ->
         end,
 
     Alarm = PrepareAlarm("Study"),
-    Alarm().
+    Alarm(),
 
+    % Using anonymous functions to work around cases where
+    % we have to pass multiple arguments to a function but are 
+    % restricted for example in map, we will pass the function 
+    % which gets executed on every element but we cannot give 
+    % it extra arguments 
+    %
+
+    io:format("funtion math:pow(2,5) : ~p~n" , [math:pow(2,5)]),
+    
+    Base = 2,
+    PowerOfTwo = fun(X) -> math:pow(Base,X) end, % notice that base is available here 
+
+    io:format("Power of 2 of [1,2,3,4] is ~p~n",[ map(PowerOfTwo,[1,2,3,4])]),
+
+    io:format("2 is even is ~p~n",[even_check(2)]),
+
+    % example of using filter , pick out the even elements in a list
+
+    io:format("Even elements in [3,1,6,2,5,8] ~p ~n",[ filter(fun higher_order_functions:even_check/1 , [3,1,6,2,5,8]) ] ).
 
 
 %example fuction is increment  and decrement
@@ -48,5 +69,32 @@ dec(X) -> X-1.
 map( _, [] ) -> [];
 map( F , [H|T] ) ->  [F(H)|map(F,T)].
 
+
+% keep even  elements in a list
+% we will use filter abstraction to implement
+% this
+
+
+even_check(Element) when Element rem 2 ==  0 ->
+    true;
+even_check(_) ->
+    false.
+
+
+
+% Filter abstraction , this simple abstraction allows to take a list 
+% apply a function to each element of the list , if they succeed ,
+% it will keep it else it will discard it.
+
+filter(TestFunction,ListOfElements) -> lists:reverse(filter(TestFunction,ListOfElements,[])).
+
+filter(_TestFunction,[],FinalList) -> FinalList;
+filter(TestFunction,[H|T],FinalList) ->
+    case TestFunction(H) of 
+            true -> 
+                filter(TestFunction,T,[H|FinalList]);
+            false ->
+                filter(TestFunction,T,FinalList)
+    end.
 
 
