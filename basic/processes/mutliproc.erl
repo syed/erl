158a2c88930955f2b170f1eb5df71efb28d5c7cd
Syed <syed1.mushtaq@gmail.com>
2012-09-27 21:21:04 +0530

    Add example for selective receive

--- basic/processes/mutliproc.erl
+++ basic/processes/mutliproc.erl
@@ -17,4 +17,18 @@ flush() ->
     end.
 
 
+important() -> 
+    receive 
+        { _From, Priority, Msg } when Priority > 10 ->
+            [ Msg | important() ] 
+    after 0 ->
+            normal()
+    end.
 
+normal() ->
+    receive 
+        { _From,Prority, Msg } -> 
+            [ Msg | normal() ]
+    after 0 -> 
+            []
+    end.
